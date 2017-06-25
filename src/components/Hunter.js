274380a621b54ee706696afb35d4f6f4ce79a085
AnastasiiaHamaiunova <asikalove@yandex.ua>
2017-06-26 00:07:04 +0300

    Form, animate

--- src/components/Hunter.js
+++ src/components/Hunter.js
@@ -4,18 +4,27 @@ import {connect} from 'react-redux';
 class Hunter extends React.Component {
     constructor(props, context){
         super(props, context);
+        this.state = this.props.hunters[this.props.match.params.id-1];
     }
     
     render(){
         return(
-            <div>
-                <img src={this.props.img}/><br/>
-                <span>{this.props.name} must be on:</span><br/>
-                
-                {this.props.x};{this.props.y}
-                <span>{this.props.gender}:</span>                
-
-                
+            <div className="profile">
+                <h3>{this.state.name}</h3>
+                <div className="profile-img"style={{backgroundImage: `url(${this.state.img})`}}>
+                </div>
+                <div className="profile-info">
+                    <div><span>Name</span>{this.state.name}</div>
+                    <div><span>Gender</span>{this.state.gender}</div>
+                    <div><span>Age</span> {this.state.age}</div>
+                    <div><span>X</span>{this.props.x}<span>Y</span>{this.props.y}</div>
+                </div> 
+                <div className="shoot">
+                    <h3>Shoot!</h3>
+                    <div className="grid">
+                        <div className="aim" style={{transform: `translate(${this.props.x}px, ${this.props.y}px)`}}></div>
+                    </div>  
+                </div>         
             </div>
         )
     }
@@ -24,8 +33,10 @@ class Hunter extends React.Component {
 const mapStateToProps= state => {
     return{
         x: state.positionState.x,
-        y: state.positionState.y
+        y: state.positionState.y,
+        hunters:state.huntersState,
     }
+
 }
 
 export default connect(mapStateToProps)(Hunter);
